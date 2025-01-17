c6bc1008da7fa96c4b75389cc56208dfb25c45ea
nikki-cmd <76265039+nikki-cmd@users.noreply.github.com>
2025-01-17 15:20:42 +0300

    Add points classification

--- generator.py
+++ generator.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+from kmeans import k_means_cluster
+import matplotlib.pyplot as plt
 
 class Point:
 	def __init__(self, x, y):
@@ -30,31 +32,46 @@ cluster1 = []
 cluster2 = []
 cluster3 = []
 
+unstaged_points = []
+
 generated = False
 while True:
-    if not generated:
-        for i in range(0, 20):
-            new_point = [pos1[0] + random.randint(-30, 30), pos1[1] + random.randint(-30, 30)]
-            points.append(Point(new_point[0], new_point[1]))
-            pygame.draw.circle(screen, "black", new_point, radius)
-            cluster1.append(new_point)
-
-        for i in range(0, 20):
-            new_point = [pos2[0] + random.randint(-30, 30), pos2[1] + random.randint(-30, 30)]
-            points.append(Point(new_point[0], new_point[1]))
-            pygame.draw.circle(screen, "black", new_point, radius)
-            cluster2.append(new_point)
-            
-        for i in range(0, 20):
-            new_point = [pos3[0] + random.randint(-30, 30), pos3[1] + random.randint(-30, 30)]
-            points.append(Point(new_point[0], new_point[1]))
-            pygame.draw.circle(screen, "black", new_point, radius)
-            cluster3.append(new_point)
-        generated = True
-            
-        pygame.display.update()
-        print(cluster1, cluster2, cluster3)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+            
+            
+        if not generated:
+            for i in range(0, 20):
+                new_point = [pos1[0] + random.randint(-30, 30), pos1[1] + random.randint(-30, 30)]
+                points.append(Point(new_point[0], new_point[1]))
+                pygame.draw.circle(screen, "red", new_point, radius)
+                cluster1.append(new_point)
+
+            for i in range(0, 20):
+                new_point = [pos2[0] + random.randint(-30, 30), pos2[1] + random.randint(-30, 30)]
+                points.append(Point(new_point[0], new_point[1]))
+                pygame.draw.circle(screen, "green", new_point, radius)
+                cluster2.append(new_point)
+                
+            for i in range(0, 20):
+                new_point = [pos3[0] + random.randint(-30, 30), pos3[1] + random.randint(-30, 30)]
+                points.append(Point(new_point[0], new_point[1]))
+                pygame.draw.circle(screen, "blue", new_point, radius)
+                cluster3.append(new_point)
+            generated = True
+                
+            pygame.display.update()
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = event.pos
+            pygame.draw.circle(screen, "black", pos, radius)
+            pygame.display.update()
+            
+            unstaged_points.append(pos)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                clusters = k_means_cluster(3, pos1, pos2, pos3, unstaged_points)
+                print('clusters:', clusters)
+            
---
+++ kmeans.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def k_means_cluster(k, c1, c2, c3, points):
+    # Initialization: choose k centroids (Forgy, Random Partition, etc.)
+    centroids = [c1, c2, c3]
+    
+    # Initialize clusters list
+    clusters = [[] for _ in range(k)]
+    
+    # Loop until convergence
+    converged = False
+    while not converged:
+        # Clear previous clusters
+        clusters = [[] for _ in range(k)]
+    
+        # Assign each point to the "closest" centroid 
+        for point in points:
+            distances_to_each_centroid = [dist(point, centroid) for centroid in centroids]
+            cluster_assignment = np.argmin(distances_to_each_centroid)
+            clusters[cluster_assignment].append(point)
+        
+        # Calculate new centroids
+        #   (the standard implementation uses the mean of all points in a
+        #     cluster to determine the new centroid)
+        '''new_centroids = [calculate_centroid(cluster) for cluster in clusters]
+        
+        converged = (new_centroids == centroids)
+        centroids = new_centroids'''
+        
+        #if converged:
+        return clusters
+
+def dist(point, centroid):
+    return np.sqrt((point[0] - centroid[0])**2 + (point[1] - centroid[1])**2)
