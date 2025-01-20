Fix centroids calculation

kmeans.py
@@ -5,12 +5,18 @@ def k_means_cluster(k, c1, c2, c3, points, cluster1, cluster2, cluster3):
     centroids = [calculate_centroid(cluster1), calculate_centroid(cluster2), calculate_centroid(cluster3)]
     
     clusters = [[], [], []]
-    
-    for point in points:
-            distances_to_each_centroid = [dist(point, centroid) for centroid in centroids]
-            cluster_assignment = np.argmin(distances_to_each_centroid)
-            clusters[cluster_assignment].append(list(point))
-    return clusters
+    done = False
+    while not done:
+        clusters = [[] for _ in range(k)]
+        for point in points:
+                distances_to_each_centroid = [dist(point, centroid) for centroid in centroids]
+                cluster_assignment = np.argmin(distances_to_each_centroid)
+                clusters[cluster_assignment].append(list(point))
+        new_centroids = [calculate_centroid(cluster) for cluster in clusters]
+        done = (centroids == new_centroids)
+        centroids = new_centroids
+        if done:
+            return clusters
 
 def dist(point, centroid):
     return np.sqrt((point[0] - centroid[0])**2 + (point[1] - centroid[1])**2)